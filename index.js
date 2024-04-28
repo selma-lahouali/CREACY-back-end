81818a5f393a2ed72917894709cbafbfc3b911f2
selma17 <lahoualiselma@gmail.com>
2024-04-28 11:58:43 +0100

    cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const AuthRoutes = require("./routes/AuthRoutes");
 const ProductsRoutes = require("./routes/ProductsRoutes");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/auth", AuthRoutes);
 app.use("/products", ProductsRoutes);
@@ -11,6 +13,6 @@ app.listen(process.env.PORT, () => {
   console.log("server is runnig");
 });
 mongoose
-  .connect(process.env.URL)
+  .connect(process.env.DB_URL)
   .then(() => console.log("connected to database"))
   .catch((err) => console.log("connection failed", err.message));
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const AuthController = require('../controllers/AuthControllers')
+const express = require("express");
+const router = express.Router();
+const AuthController = require("../controllers/AuthControllers");
 
-router.post("/register",AuthController.register)
-router.post("/login",AuthController.login)
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
 
 module.exports = router;
