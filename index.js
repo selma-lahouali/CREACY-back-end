3c72809566f9f724951b09db57d3e0fb117067be
selma17 <lahoualiselma@gmail.com>
2024-05-13 19:51:42 +0100

    fix port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use("/shop", ShopRoutes);
 app.use("/products", ProductsRoutes);
 
 // port
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server is runnig");
 });
 // database
